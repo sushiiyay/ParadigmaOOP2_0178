7c8faee851874dea27298730393f30ab36468991
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-18 14:46:43 +0700

    Menambahkan cout di class pelajar

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -14,5 +14,18 @@ public:
 
 class pekerja : public orang {
 public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
 
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n"
+    }
 }
