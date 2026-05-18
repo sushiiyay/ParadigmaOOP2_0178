a93567f89ac8f94c529d210ce085b0d39d74dbc5
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-18 14:53:07 +0700

    Menambahkan class pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -18,7 +18,7 @@ public:
     {
         cout << "pekerja dibuat\n" << endl;
     }
-}
+};
 
 class pelajar : virtual public orang {
 public: 
@@ -27,7 +27,7 @@ public:
     {
         cout << "pelajar dibuat\n" << endl;
     }
-};
+}
 
 class budi : public pekerja, public pelajar {
 public:
