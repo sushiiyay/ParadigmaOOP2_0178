287c217598dbd18ec7ec478b78ef76d75b0f0f8d
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-18 14:29:09 +0700

    Menambahkan void display

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -7,5 +7,10 @@ private: string x, y;
 public:
     // method untuk mengisi nilai
     // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
     
 }
